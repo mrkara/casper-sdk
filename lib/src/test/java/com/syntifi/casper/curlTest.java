bc3a6e2d4a8e8f38849f0deaf32593d890beb1d8
AB <andre@syntifi.com>
2021-07-16 11:47:30 +0200

    adding tests to check transfer methods

--- lib/src/test/java/com/syntifi/casper/curlTest.java
+++ lib/src/test/java/com/syntifi/casper/curlTest.java
@@ -6,11 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.stream.Collectors;
 
-import com.syntifi.casper.Casper;
 import com.syntifi.casper.model.chain.get.block.CasperBlock;
 import com.syntifi.casper.model.chain.get.block.CasperBlockResponse;
+import com.syntifi.casper.model.chain.get.block.transfer.CasperTransfer;
+import com.syntifi.casper.model.chain.get.block.transfer.CasperTransferResponse;
+
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -29,10 +32,6 @@ public class curlTest {
         mapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
     }
     
-    private void print(String output) {
-        System.out.println(output);
-    }
-
     private String curl(String[] command) throws IOException, InterruptedException {
         Process process = Runtime.getRuntime().exec(command);
         process.waitFor();
@@ -43,7 +42,7 @@ public class curlTest {
     }
 
     @Test
-    void getLastBlock() throws IOException, InterruptedException{
+    void getLastBlock() throws IOException, InterruptedException {
         CasperBlock lastBlock = casper.getBlock();
         lastBlockParent = lastBlock.header.parentHash;
         lastBlockHeight = lastBlock.header.height;
@@ -57,7 +56,7 @@ public class curlTest {
     }
 
     @Test
-    void getBlockByHeight() throws IOException, InterruptedException{
+    void getBlockByHeight() throws IOException, InterruptedException {
         long height = lastBlockHeight - 5;
         CasperBlock block = casper.getBlock(height);
         String[] command = {"curl", "-X",  "POST",  "http://"+ ip + ":" + port + "/rpc",
@@ -71,7 +70,7 @@ public class curlTest {
     }
 
     @Test
-    void getBlockByHash() throws IOException, InterruptedException{
+    void getBlockByHash() throws IOException, InterruptedException {
         CasperBlock block = casper.getBlock(lastBlockParent);
         String[] command = {"curl", "-X",  "POST",  "http://"+ ip + ":" + port + "/rpc",
                     "-H", "Content-Type:application/json", "-d", 
@@ -84,7 +83,7 @@ public class curlTest {
     }
 
     @Test
-    void getEndEraBlock() throws IOException, InterruptedException{
+    void getEndEraBlock() throws IOException, InterruptedException {
         long height = 106;
         CasperBlock block = casper.getBlock(height);
         String[] command = {"curl", "-X",  "POST",  "http://"+ ip + ":" + port + "/rpc",
@@ -95,5 +94,44 @@ public class curlTest {
         var obj = mapper.treeToValue(mapper.readTree(curlResponse), CasperBlockResponse.class);
         assertEquals(mapper.readTree(mapper.writeValueAsString(obj.result.block)), 
                      mapper.readTree(mapper.writeValueAsString(block)));
+    }
+
+    @Test
+    void getLastBlockTransfers() throws IOException, InterruptedException {
+        List<CasperTransfer> transfers = casper.getTransfers();
+        String[] command = {"curl", "-X",  "POST",  "http://"+ ip + ":" + port + "/rpc",
+                    "-H", "Content-Type:application/json", "-d", 
+                    "{\"jsonrpc\":\"2.0\",\"id\":\"1\",\"method\":\"chain_get_block_transfers\"}"};
+        String curlResponse = curl(command);
+        var obj = mapper.treeToValue(mapper.readTree(curlResponse), CasperTransferResponse.class);
+        assertEquals(mapper.readTree(mapper.writeValueAsString(obj.result.transfers)), 
+                     mapper.readTree(mapper.writeValueAsString(transfers)));
+    }
+
+    @Test
+    void getTransfersByBlockHeight() throws IOException, InterruptedException {
+        long height = lastBlockHeight - 5;
+        List<CasperTransfer> transfers = casper.getTransfers(height);
+        String[] command = {"curl", "-X",  "POST",  "http://"+ ip + ":" + port + "/rpc",
+                    "-H", "Content-Type:application/json", "-d", 
+                    "{\"jsonrpc\":\"2.0\",\"id\":\"2\",\"method\":\"chain_get_block_transfers\", " +
+                    "\"params\":{\"block_identifier\":{\"Height\": " + height + "}}}"};
+        String curlResponse = curl(command);
+        var obj = mapper.treeToValue(mapper.readTree(curlResponse), CasperTransferResponse.class);
+        assertEquals(mapper.readTree(mapper.writeValueAsString(obj.result.transfers)), 
+                     mapper.readTree(mapper.writeValueAsString(transfers)));
+    }
+
+    @Test
+    void getTransfersByBlockByHash() throws IOException, InterruptedException {
+        List<CasperTransfer> transfers = casper.getTransfers(lastBlockParent);
+        String[] command = {"curl", "-X",  "POST",  "http://"+ ip + ":" + port + "/rpc",
+                    "-H", "Content-Type:application/json", "-d", 
+                    "{\"jsonrpc\":\"2.0\",\"id\":\"3\",\"method\":\"chain_get_block_transfers\", " +
+                    "\"params\":{\"block_identifier\":{\"Hash\": \"" + lastBlockParent + "\"}}}"};
+        String curlResponse = curl(command);
+        var obj = mapper.treeToValue(mapper.readTree(curlResponse), CasperTransferResponse.class);
+        assertEquals(mapper.readTree(mapper.writeValueAsString(obj.result.transfers)), 
+                     mapper.readTree(mapper.writeValueAsString(transfers)));
     }
 }
