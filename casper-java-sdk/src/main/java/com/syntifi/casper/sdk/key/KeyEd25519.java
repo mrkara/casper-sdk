576f41b61d053e0f85166b47e14274823cb961e7
AB <andre@syntifi.com>
2021-09-02 13:28:33 +0200

    small changes on Ed25519

--- casper-java-sdk/src/main/java/com/syntifi/casper/sdk/key/KeyEd25519.java
+++ casper-java-sdk/src/main/java/com/syntifi/casper/sdk/key/KeyEd25519.java
@@ -30,7 +30,8 @@ public class KeyEd25519 {
 
     private Ed25519PrivateKeyParameters privateKey; 
     private Ed25519PublicKeyParameters publicKey; 
-    private byte[] keyId;
+    //1.3.101.112 is the OID identifier for the Ed25519 schema 
+    private static final ASN1ObjectIdentifier OID = new ASN1ObjectIdentifier("1.3.101.112");
 
     private byte[] readPemFile(String filename) throws IOException {
         try (FileReader keyReader = new FileReader(filename);
@@ -46,11 +47,6 @@ public class KeyEd25519 {
         }
     }
 
-    //1.3.101.112 is the OID identifier for the Ed25519 schema 
-    private ASN1ObjectIdentifier getDERPrefix() {
-        return new ASN1ObjectIdentifier("1.3.101.112");
-    }
-
     /*
     SEQUENCE (3 elem)
       INTEGER 0
@@ -60,16 +56,16 @@ public class KeyEd25519 {
         OCTET STRING (32 byte) 38AECE974291F14B5FEF97E1B21F684394120B6E7A8AFB04398BBE787E8BC559
     */
     public void readPrivateKey(String filename) throws IOException {
-        ASN1Primitive derKey = ASN1Primitive.fromByteArray(readPemFile(filename));
-        var keyInfo = PrivateKeyInfo.getInstance(derKey);
+        ASN1Primitive key = ASN1Primitive.fromByteArray(readPemFile(filename));
+        var keyInfo = PrivateKeyInfo.getInstance(key);
         String algoId = keyInfo.getPrivateKeyAlgorithm().getAlgorithm().toString();
-        if (algoId.equals("1.3.101.112")) {
+        if (algoId.equals(OID.getId())) {
             privateKey = new Ed25519PrivateKeyParameters(keyInfo.getPrivateKey().getEncoded(), 4);
         }
     }
 
     public void writePrivateKey(String filename) throws IOException {
-        var derPrefix = new DERSequence(getDERPrefix());
+        var derPrefix = new DERSequence(OID);
         var key = new DEROctetString(new DEROctetString(privateKey.getEncoded()));
         var vector = new ASN1EncodableVector();
         vector.add(new ASN1Integer(0));
@@ -89,14 +85,14 @@ public class KeyEd25519 {
         ASN1Primitive derKey = ASN1Primitive.fromByteArray(readPemFile(filename));
         var objBaseSeq = ASN1Sequence.getInstance(derKey);
         String objId = ASN1ObjectIdentifier.getInstance(ASN1Sequence.getInstance(objBaseSeq.getObjectAt(0)).getObjectAt(0)).getId();
-        if (objId.equals("1.3.101.112")) {
+        if (objId.equals(OID.getId())) {
             var key = DERBitString.getInstance(objBaseSeq.getObjectAt(1));
             publicKey = new Ed25519PublicKeyParameters(key.getBytes(), 0);
         }
     }
 
     public void writePublicKey(String filename) throws IOException {
-        var derPrefix = new DERSequence(getDERPrefix());
+        var derPrefix = new DERSequence(OID);
         var key = new DERBitString(publicKey.getEncoded());
         var vector = new ASN1EncodableVector();
         vector.add(derPrefix);
