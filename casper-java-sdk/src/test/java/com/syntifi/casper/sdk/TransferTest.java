47566eebf2551f2852974e2920dc1ceafe727231
AB <andre@syntifi.com>
2021-10-06 15:12:01 +0200

    adding de/serialization TransferData

--- casper-java-sdk/src/test/java/com/syntifi/casper/sdk/TransferTest.java
+++ casper-java-sdk/src/test/java/com/syntifi/casper/sdk/TransferTest.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.syntifi.casper.sdk.model.block.JsonBlock;
-import com.syntifi.casper.sdk.model.block.JsonBlockData;
+import com.syntifi.casper.sdk.model.transfer.Transfer;
+import com.syntifi.casper.sdk.model.transfer.TransferData;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -16,13 +16,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Unit tests for {@link JsonBlockDataTest}
+ * Unit tests for {@link TransferData}
  * 
  * @author Alexandre Carvalho
  * @author Andre Bertolace
  * @since 0.0.1
  */
-public class JsonBlockDataTest extends AbstractJsonTests {
+public class TransferTest extends AbstractJsonTests {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StatusTests.class);
 
@@ -35,16 +35,16 @@ public class JsonBlockDataTest extends AbstractJsonTests {
 
     @Test
     void test_era_end_block() throws JsonMappingException, JsonProcessingException, IOException {
-        //curl -X POST -H 'Content-Type: application/json' -d '{"id":"0","jsonrpc":"2.0","method":"chain_get_block", "params":{"block_identifier":{"Height":"246762"}}}' http://nodeIP:7777/rpc 
-        String inputJson = getPrettyJson(loadJsonFromFile("block-samples/block-end-era.json"));
+        //curl -X POST -H 'Content-Type: application/json' -d '{"id":"0","jsonrpc":"2.0","method":"chain_get_block_transfers","params":{"block_identifier":{"Height":198148}}}'  http://195.201.142.76:7777/rpc
+        String inputJson = getPrettyJson(loadJsonFromFile("transfer-samples/transfer.json"));
 
         LOGGER.debug("Original JSON: {}", inputJson);
 
-        JsonBlockData block = OBJECT_MAPPER.readValue(inputJson, JsonBlockData.class);
+        TransferData transfer = OBJECT_MAPPER.readValue(inputJson, TransferData.class);
 
-        assertTrue(block.getBlock() instanceof JsonBlock);
+        assertTrue(transfer.getTransfers().get(0) instanceof Transfer);
 
-        String reserializedJson = getPrettyJson(block);
+        String reserializedJson = getPrettyJson(transfer);
 
         LOGGER.debug("Serialized JSON: {}", reserializedJson);
 
