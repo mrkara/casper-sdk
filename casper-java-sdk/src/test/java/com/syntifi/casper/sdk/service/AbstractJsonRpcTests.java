c927cfc6fed4180faaf0b834c14145bd260e7967
Alexandre Carvalho <adcarvalho@gmail.com>
2021-10-06 11:59:00 -0300

    ip main node

--- casper-java-sdk/src/test/java/com/syntifi/casper/sdk/service/AbstractJsonRpcTests.java
+++ casper-java-sdk/src/test/java/com/syntifi/casper/sdk/service/AbstractJsonRpcTests.java
@@ -19,7 +19,7 @@ public abstract class AbstractJsonRpcTests {
 	@Getter
 	@AllArgsConstructor(access = AccessLevel.PRIVATE)
 	public enum CasperNetwork {
-		MAIN_NET("45.76.251.225", 7777), TEST_NET("144.76.97.151", 7777);
+		MAIN_NET("195.201.142.76", 7777), TEST_NET("144.76.97.151", 7777);
 
 		private String ip;
 		private int port;
