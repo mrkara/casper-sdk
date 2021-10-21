0d81e77dfb63147d9d6fccc40c07ffe031d67af4
Alexandre Carvalho <adcarvalho@gmail.com>
2021-10-21 14:48:14 -0300

    removed localnet

--- src/test/java/com/syntifi/casper/sdk/service/AbstractJsonRpcTests.java
+++ src/test/java/com/syntifi/casper/sdk/service/AbstractJsonRpcTests.java
@@ -18,27 +18,23 @@ import lombok.Getter;
  * @since 0.0.1
  */
 public abstract class AbstractJsonRpcTests extends AbstractJsonTests {
-	@Getter
-	@AllArgsConstructor(access = AccessLevel.PRIVATE)
-	public enum CasperNetwork {
-		MAIN_NET("195.201.142.76", 7777), TEST_NET("65.21.238.244", 7777), LOCAL_NET("csprnd.ddns.net", 11101);
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public enum CasperNetwork {
+        MAIN_NET("195.201.142.76", 7777), TEST_NET("65.21.238.244", 7777);
 
-		private String ip;
-		private int port;
-	}
+        private String ip;
+        private int port;
+    }
 
-	protected static CasperService casperServiceMainnet;
-	protected static CasperService casperServiceTestnet;
-	protected static CasperService casperServiceLocalnet;
+    protected static CasperService casperServiceMainnet;
+    protected static CasperService casperServiceTestnet;
 
-	@BeforeAll
-	public static void setUp() throws MalformedURLException {
-		casperServiceMainnet = CasperService.usingPeer(CasperNetwork.MAIN_NET.getIp(),
-				CasperNetwork.MAIN_NET.getPort());
-		casperServiceTestnet = CasperService.usingPeer(CasperNetwork.TEST_NET.getIp(),
-				CasperNetwork.TEST_NET.getPort());
-		casperServiceLocalnet = CasperService.usingPeer(CasperNetwork.LOCAL_NET.getIp(),
-				CasperNetwork.LOCAL_NET.getPort());
-	}
-
+    @BeforeAll
+    public static void setUp() throws MalformedURLException {
+        casperServiceMainnet = CasperService.usingPeer(CasperNetwork.MAIN_NET.getIp(),
+                CasperNetwork.MAIN_NET.getPort());
+        casperServiceTestnet = CasperService.usingPeer(CasperNetwork.TEST_NET.getIp(),
+                CasperNetwork.TEST_NET.getPort());
+    }
 }
